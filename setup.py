c8014a722bae68e1b386080d7d1cf67a8d21832c
Resousse <11071770+Resousse@users.noreply.github.com>
2023-11-27 16:58:16 +0100

    1.0.5 version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="powerbi-parser",
-    version="1.0.4",
+    version="1.0.5",
     description="Parser Power BI files to browse assets",
     long_description=README,
     long_description_content_type="text/markdown",
