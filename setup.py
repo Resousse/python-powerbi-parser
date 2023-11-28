74acc357bbfb706c4be7d0a3520827fdcd6d255b
Resousse <11071770+Resousse@users.noreply.github.com>
2023-11-28 12:58:12 +0100

    refactoring for testing

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="powerbi-parser",
-    version="1.0.6",
+    version="1.0.7",
     description="Parser Power BI files to browse assets",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -17,11 +17,7 @@ setup(
     license="Apache 2.0",
     classifiers=[
     ],
-    package_dir = {'powerBIParser' : './'},
     packages=["powerBIParser"],
-    package_data = {
-        'powerBIParser': ['./LICENSE', './README.md', '*.py']
-    },
     exclude_package_data={'powerBIParser' : ["setup.py"]},
     include_package_data=True
 )
